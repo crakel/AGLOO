5cd281cc8fd3a7ba659514004e4bde1589f41bc8
root <crakel528@gmail.com>
2021-03-09 14:43:19 +0900

    README 수정

--- back-end/README.md
+++ back-end/README.md
@@ -1,15 +1,23 @@
-# back-end
-개발 및 공부 과정
+# AGLOO / Back-End
 
-Node.js 기반 API Server 
+## 개발 및 공부 과정
 
-초기 개발 단계에서 테스트용으로 웹 views 연동
+Node.js 기반 API Server    
+DB : MySQL
+
+초기 개발 단계에서 테스트용으로 웹 views 연동   
 -> 현재 React Native 사용한 app-front에서 테스트
 
 사용한 모듈
-express.js
-nodemon
-pm2
+  - express.js     
+  - nodemon
+  - pm2
+  - dotenv
+  - bcrypt
+  - jwt
+  - multer
+
+<br>
 
 ## MySQL 쿼리 변수담기
 ```javascript
@@ -19,9 +27,18 @@ sql = mysql.format(sql, inserts);
 ```
 ?? 로 테이블, ?로 필드를 변수 매핑할 수 있다.
 
+<br>
+
+## SQL 결과 값 RowDataPacket을 변환하는 방법들
+```javascript
+JSON.stringify() // JSON 객체를 String 객체로 변환
+JSON.parse() // String 객체를 JSON 객체로 변환
+```
+참고 : <https://stackoverflow.com/questions/31221980/how-to-access-a-rowdatapacket-object>
+
 
 ## Multer 저장경로, 파일명 설정
-multer 모듈을 통해서 post로 전송된 파일의 저장경로와 파일명 등을 처리하기 위해서는 DiskStorage 엔진이 필요하다.
+multer 모듈을 통해서 post로 전송된 파일의 저장경로와 파일명 등을 처리하기 위해서는 DiskStorage 엔진이 필요하다.   
 참고 : <https://github.com/expressjs/multer#storage>
 ```javascript
 var multer = require('multer'); // multer모듈 적용 (for 파일업로드)
--- back-end/app/app.js
+++ back-end/app/app.js
@@ -1,19 +1,3 @@
-// const http = require("http");
-// const app = http.createServer((req, res) => {
-//     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-//     if (req.url === "/") {
-//         res.end("여기는 루트 입니다.");
-//     } else if (req.url === "/login") {
-//         res.end("여기는 로그인 화면입니다.");
-//     }
-// });
-
-// app.listen(3001, () => {
-//     console.log("http로 가동된 서버입니다.");
-// });
-
-// MVC 패턴 
-// app.js main file
 "use strict";
 
 // 모듈
@@ -27,15 +11,15 @@ const app = express();
 const home = require("./src/routes/home");
 // 앱 세팅
 app.set("views", "./src/views");
-app.set("view engine", "ejs"); // HTML과 비슷
+app.set("view engine", "ejs");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // URL 한글, 공백 문자 인식 해결
-app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
+app.use("/", home);
 app.use(express.static(`${__dirname}/src/public`)) // 정적 경로 추가 
 // dirname -> 현재 위치 
 
 module.exports = app;
 
-// npm init -y(자동완성) -> package.json 생
+// npm init -y(자동완성) -> package.json 생성
